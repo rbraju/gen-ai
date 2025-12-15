3577d19c52cfcd6acb2fa548d0a5dafc0b5a3ac5
Rajesh <rbhoopathyraju@upgrade.com>
2025-12-14 23:23:26 -0800

    Fix imports

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,17 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.chains.question_answering import load_qa_chain
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
 
-OPENAI_API_KEY = "<OPEN_API_KEY>"
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain.chains import RetrievalQA
+
+OPENAI_API_KEY = "<OPENAI_API_KEY>"
 
 # upload PDF files
 st.header("My First Chatbot")
@@ -18,9 +23,11 @@ with st.sidebar:
 # Extract the text
 if file is not None:
     pdfReader = PdfReader(file)
-    text = ""
-    for page in pdfReader.pages:
-        text += page.extract_text()
+    # text = ""
+    # for page in pdfReader.pages:
+    #     text += page.extract_text()
+    text = "".join([page.extract_text() for page in pdfReader.pages])
+
 
     # Break it into chunks
     text_splitter = RecursiveCharacterTextSplitter(
@@ -40,7 +47,6 @@ if file is not None:
     # Get user question
     question = st.text_input("Type your question here")
 
-
     # Do similarity search
     if question:
         match = vector_store.similarity_search(question)
@@ -52,6 +58,11 @@ if file is not None:
             max_tokens=500,
             model_name="gpt-3.5-turbo"
         )
-        chain = load_qa_chain(llm, chain_type="stuff")
+        # chain = load_qa_chain(llm, chain_type="stuff")
+        chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            retriever=vector_store.as_retriever(),
+            chain_type="stuff"
+        )
         output = chain.run(input_documents=match, question=question)
         st.write(output)
---
+++ sanity-check.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version)
+
+import streamlit as st
+
+from PyPDF2 import PdfReader
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+print("ALL IMPORTS OK")
